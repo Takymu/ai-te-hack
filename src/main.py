a158e24f90fe69d2eaa6cdd76b399875f664a64a
Bogdan Martynov <85843337+Hom4ikTop4ik@users.noreply.github.com>
2025-09-29 14:08:32 +0700

    Update main.py
    Обновил вслед за addovals.py

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from diffusion import generate_image
 # from gigachat import generate_image
 
 from actor_critic import generate_comix_actcrit
-from yolo_detect import detect_faces
-from addovals import add_speech_bubble
+from yolo_detect import detect_faces, detect_faces_full
+from addovals import add_speech_bubbles_multiple
 from imgcombine import combine_images_to_file
 
 doctext = extract_text_from_pdf('../data/Правила записи иа первичный прием, ЦПК ФТС.pdf')
@@ -47,7 +47,7 @@ for i in range(len(scenario['scenes'])):
 
     img, ext = generate_image(sceneprompt)
     print(sceneprompt)
-    faces = detect_faces(img)
+    faces = detect_faces_full(img)
     left = True
 
     if len(faces) == 2 and faces[0][0] > faces[1][0]:
@@ -55,18 +55,41 @@ for i in range(len(scenario['scenes'])):
         faces[0] = faces[1]
         faces[1] = face
 
-    for x, y in faces:
+    bubels = []
+    UP_HEAD = 0
+    for x1, y1, x2, y2 in faces:
+        x = (x1+x2)//2
+        y = (y1+y2)//2
+        UP_HEAD += y
+
         if left and charLaction is not None:
-            img = add_speech_bubble(img, charLaction, x, y,
-                               max_bubble_width=280,
-                                 min_font_size=8)
+            bubels.append({
+                "text": charLaction,
+                "head_coords": (x, y)
+            })
             left = False
         elif charRaction is not None:
-            img = add_speech_bubble(img, charRaction, x, y,
-                               max_bubble_width=280,
-                                 min_font_size=8)
+            bubels.append({
+                "text": charRaction,
+                "head_coords": (x, y)
+            })
+
+    UP_HEAD //= len(faces)
+
+    add_speech_bubbles_multiple(
+        img, bubels,
+        MAX_BUBBLE_WIDTH=280,
+        MIN_FONT_SIZE=8,
+        BORDER_COLOR = "black",
+        FILL_COLOR = "white",
+        TEXT_COLOR = "black",
+        BORDER_WIDTH = 3,
+        UP_HEAD=UP_HEAD,
+        RATIO_HEAD=0.5,
+        RATIO_ELLIPSE=0.5
+    )
     scenelist.append(img)
 
 
-imgname = 'comix.jpg'
+imgname = 'comics.jpg'
 combine_images_to_file(scenelist, imgname)
