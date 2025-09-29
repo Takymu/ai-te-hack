d901b1c051aee275b56c3d1697be75d43e4be0e2
Bogdan Martynov <85843337+Hom4ikTop4ik@users.noreply.github.com>
2025-09-29 14:07:33 +0700

    Update yolo_detect.py
    Добавил неурезанный вариант функции detect_faces_full

--- src/yolo_detect.py
+++ src/yolo_detect.py
@@ -22,3 +22,22 @@ def detect_faces(image_bytes):
                 coords.append((x_center, y_center))
     
     return coords
+
+def detect_faces_full(image_bytes):
+    model = YOLO('../weights/yolov8x6_animeface.pt')
+
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    results = model(image_rgb, conf=0.5)
+    coords = []
+    
+    for result in results:
+        boxes = result.boxes
+        if boxes is not None:
+            for box in boxes:
+                xyxy = box.xyxy[0].cpu().numpy()
+                coords.append(xyxy)
+    
+    return coords
